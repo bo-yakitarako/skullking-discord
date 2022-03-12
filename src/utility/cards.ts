26142fa58a00d569a0f72a121c79df161c380161
bo-yakitarako <bo-yakitarako@users.noreply.github.com>
2022-03-12 19:49:07 +0900

    デッキ作れた！

---
+++ src/utility/cards.ts
@@ -0,0 +1,61 @@
+type Color = 'green' | 'yellow' | 'purple' | 'black';
+type ColorCard = {
+  color: Color;
+  number: number;
+  bonus: number;
+};
+
+type Special = 'skullking' | 'pirates' | 'mermaids' | 'escape' | 'tigres';
+type SpecialCard = {
+  type: Special;
+  escapeType?: 'standard' | 'gold' | 'kraken';
+};
+
+export type Card = ColorCard | SpecialCard;
+
+const generate_bonus = (type: Color | Special, number: number) => {
+  if (number < 14) {
+    return 0;
+  }
+  return type === 'black' ? 20 : 10;
+};
+
+const shuffle = (cards: Card[]) => {
+  let shuffledCards: Card[] = [];
+  while (cards.length > 0) {
+    const index = Math.floor(Math.random() * cards.length);
+    shuffledCards = [...shuffledCards, cards[index]];
+    cards.splice(index, 1);
+  }
+  return shuffledCards;
+};
+
+export const generate_deck = () => {
+  const colors: Color[] = ['green', 'yellow', 'purple', 'black'];
+  const colorCards = colors.reduce((prev, color) => {
+    const numberCards = [...Array(14)].map((_, index) => {
+      const number = index + 1;
+      const bonus = generate_bonus(color, number);
+      return { color, number, bonus };
+    });
+    return [...prev, ...numberCards];
+  }, [] as ColorCard[]);
+
+  const specialCards: SpecialCard[] = [
+    { type: 'skullking' },
+    ...[...Array(5)].map<SpecialCard>(() => ({ type: 'pirates' })),
+    ...[...Array(2)].map<SpecialCard>(() => ({ type: 'mermaids' })),
+    { type: 'tigres' },
+    ...[...Array(5)].map<SpecialCard>(() => ({
+      type: 'escape',
+      escapeType: 'standard',
+    })),
+    ...[...Array(2)].map<SpecialCard>(() => ({
+      type: 'escape',
+      escapeType: 'gold',
+    })),
+    { type: 'escape', escapeType: 'kraken' },
+  ];
+
+  return shuffle([...colorCards, ...specialCards]);
+};
